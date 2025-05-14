54c6e9ebb029b434105be3c68ab5b3f005686b53
BlakeA494 <blake.n.adams@gmail.com>
2025-05-13 21:11:00 -0400

    Update ratpool app.py

---
+++ ratpool app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+from masters_prop_pool import evaluate_pool
+
+st.title("🏆 Masters 2025 Prop Pool Leaderboard")
+
+pool = st.selectbox("Select Pool", ["Queens", "Framily"])
+
+if st.button("Show Leaderboard"):
+    evaluate_pool(pool)
+
