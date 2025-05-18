bc1c86fee1b716a0784cd5f3e52fb5673bc8ce63
BlakeA494 <blake.n.adams@gmail.com>
2025-05-17 20:20:17 -0400

    Update ratpool app.py

--- ratpool app.py
+++ ratpool app.py
@@ -5,9 +5,9 @@ from poolcode import active_pool
 
 st.title("🏆 PGA Championship 2025 Pool Leaderboard")
 
-active_pool = st.selectbox("Select Pool", ["Queen's", "Framily"])
+pool_choice = st.selectbox("Select Pool", ["Queen's", "Framily"])
 
 if st.button("Show Leaderboard"):
-    evaluate_pool(active_pool)
+    evaluate_pool(pool_choice)
 
-display_pickem_results(active_pool)
+display_pickem_results(pool_choice)
